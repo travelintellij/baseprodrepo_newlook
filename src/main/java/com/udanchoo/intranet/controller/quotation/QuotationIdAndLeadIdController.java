7dee8de0f7dfacde56fff60d2336bb38bd0646c7
Himanshu Chauhan <101518009+hi-manss@users.noreply.github.com>
2024-05-29 15:03:04 +0530

    Quotation_Id methods changes.

--- src/main/java/com/udanchoo/intranet/controller/quotation/QuotationIdAndLeadIdController.java
+++ src/main/java/com/udanchoo/intranet/controller/quotation/QuotationIdAndLeadIdController.java
@@ -1,31 +1,19 @@
 package com.udanchoo.intranet.controller.quotation;
 
-import com.udanchoo.intranet.model.quotation.ReactQuotationIdObj;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 @Controller
-@RestController
-
 public class QuotationIdAndLeadIdController {
-    @Autowired
-    private ReactQuotationIdObj reactQuotationIdObj;
 
+    @Value("${redirect.url}")
+    private String redirectUrl;
 
     @PostMapping("/storeIds")
     public RedirectView storeIds(@RequestParam String leadId, @RequestParam String quotationId) {
-        reactQuotationIdObj.setLeadId(leadId);
-        reactQuotationIdObj.setQuotationId(quotationId);
-
-        return new RedirectView("http://localhost:5173"); // Redirect to the specified URL
-    }
-
-    @GetMapping("/api/getIds")
-    @ResponseBody
-    public ReactQuotationIdObj getIds() {
-        return reactQuotationIdObj;
+        String fragment = "#leadId=" + leadId + "&quotationId=" + quotationId;
+        return new RedirectView(redirectUrl + fragment);
     }
 }
